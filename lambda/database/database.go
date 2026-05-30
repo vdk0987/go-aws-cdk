39fd3bcf3464b14902e4140bb3bce6adb7ce8f8b
vdk0987 <vdk0987@gmail.com>
2026-05-30 22:43:50 +0530

    user validation function

--- lambda/database/database.go
+++ lambda/database/database.go
@@ -3,8 +3,14 @@ package database
 import (
 	"context"
 
+	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb/types"
+)
+
+const (
+	TABLE_NAME = "userTable"
 )
 
 type DynamoDBClient struct {
@@ -21,4 +27,25 @@ func NewDynamoDBClient() (*DynamoDBClient, error) {
 	return &DynamoDBClient{
 		databasestore: db,
 	}, nil
+}
+
+func (u *DynamoDBClient) UserValidation(username string) (bool, error) {
+	result, err := u.databasestore.GetItem(
+		context.Background(),
+		&dynamodb.GetItemInput{
+			TableName: aws.String(TABLE_NAME),
+			Key: map[string]types.AttributeValue{
+				username: &types.AttributeValueMemberS{
+					Value: username,
+				},
+			},
+		},
+	)
+	if err != nil {
+		return true, err
+	}
+	if result.Item == nil {
+		return false, nil
+	}
+	return true, nil
 }

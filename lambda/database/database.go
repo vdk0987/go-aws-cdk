9c15a5386a68fbf45214513cf0f48e893441c5d9
vdk0987 <vdk0987@gmail.com>
2026-06-12 16:25:03 +0530

    add insert user function

--- lambda/database/database.go
+++ lambda/database/database.go
@@ -3,6 +3,8 @@ package database
 import (
 	"context"
 
+	lambda_types "lambda-func/types"
+
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
@@ -48,4 +50,23 @@ func (u *DynamoDBClient) UserValidation(username string) (bool, error) {
 		return false, nil
 	}
 	return true, nil
+}
+
+func (u *DynamoDBClient) InsertUser(user lambda_types.RegisterUser) error {
+	item := &dynamodb.PutItemInput{
+		TableName: aws.String(TABLE_NAME),
+		Item: map[string]types.AttributeValue{
+			"username": &types.AttributeValueMemberS{
+				Value: user.Username,
+			},
+			"password": &types.AttributeValueMemberS{
+				Value: user.Password,
+			},
+		},
+	}
+	_, err := u.databasestore.PutItem(context.Background(), item)
+	if err != nil {
+		return err
+	}
+	return nil
 }

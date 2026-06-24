d16a07b09cc80db1eee11a3a28c9a53d856d69fa
vdk0987 <vdk0987@gmail.com>
2026-06-24 21:16:48 +0530

    create function for registering user

--- lambda/api/api.go
+++ lambda/api/api.go
@@ -1,7 +1,9 @@
 package api
 
 import (
+	"fmt"
 	"lambda-func/database"
+	"lambda-func/types"
 )
 
 type ApiHandler struct {
@@ -9,7 +11,27 @@ type ApiHandler struct {
 }
 
 func NewApiHandler(dbStore *database.DynamoDBClient) ApiHandler {
-	return ApiHandler {
+	return ApiHandler{
 		dbStore: dbStore,
 	}
+}
+
+func (api *ApiHandler) RegisterUser(event types.RegisterUser) error {
+	if event.Password == "" || event.Username == "" {
+		return fmt.Errorf("Username and Password fields cannot be empty")
+	}
+	doesUserExist, err := api.dbStore.UserValidation(event.Username)
+
+	if err != nil {
+		return fmt.Errorf("Error while registering user %w", err)
+	}
+	if doesUserExist {
+		return fmt.Errorf("Username is already in use")
+	}
+
+	err = api.dbStore.InsertUser(event)
+	if err != nil {
+		return fmt.Errorf("Error inserting the user into db %w", err)
+	}
+	return nil
 }
